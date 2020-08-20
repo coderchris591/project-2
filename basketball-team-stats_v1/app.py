4b35a74423b0b93974d49a3a2a02216bc3aaf046
Chris Martinz <chmartinez2014@gmail.com>
2020-08-20 17:49:12 -0500

    Finished

--- basketball-team-stats_v1/app.py
+++ basketball-team-stats_v1/app.py
@@ -6,15 +6,17 @@ import random
 players = copy.deepcopy(PLAYERS)
 teams = copy.deepcopy(TEAMS)
 num_players_team = int(round(len(players) / len(teams)))
+
+
+
 if __name__ == "__main__":
+
     def clean_data():
         for player in players:
             s = player['height'].split()
             for number in s:
                 if number.isdigit():
                     heights = int(number)
-
-
         for player in players:
             if player['experience'] == 'YES':
                 experience = True
@@ -40,7 +42,7 @@ if __name__ == "__main__":
             try:
                 choice = int(input(' \n 1) Display Team stats \n 2) Quit \n\n Enter an option >  '))
             except ValueError as e:
-                print('Please choose 1 or 2')
+                print(' \nPlease choose 1 or 2')
             else:
                 if choice == 1:
                     print('\n 1) Panthers \n\n 2) Bandits \n\n 3) Warriors')
@@ -52,19 +54,21 @@ if __name__ == "__main__":
                         if team_select > 3 or team_select < 1:
                             print("Please choose team 1, 2 or 3")
                         elif team_select == 1:
-                            print("\nTeam: Panthers \n\n Number of Players:", num_players_team, "\n\n Players:")
+                            print(" \nTeam: Panthers \n\n Number of Players:", num_players_team, "\n\n Players:")
                             print(*player_list[0:6], sep=', ')
                         elif team_select == 2:
-                            print("\nTeam: Bandits \n\n Number of Players:", num_players_team, "\n\n Players:")
+                            print(" \nTeam: Bandits \n\n Number of Players:", num_players_team, "\n\n Players:")
                             print(*player_list[6:12], sep=', ')
                         elif team_select == 3:
-                            print("\nTeam: Warriors \n\n Number of Players:", num_players_team, "\n\n Players:")
+                            print(" \nTeam: Warriors \n\n Number of Players:", num_players_team, "\n\n Players:")
                             print(*player_list[12:18], sep=', ')
-                    cont = input('\nPress ENTER to continue...')
-
+                    try:
+                        cont = input('\nPress ENTER to continue...')
+                    except SyntaxError as e:
+                        pass
                 elif choice == 2:
                     print('quitting...')
                     quit()
                 else:
-                    print('Thats not a valid option')
+                    print('Oops, thats not a 1 or a 2 -- try again')
     start()
